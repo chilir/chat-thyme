450f0e560d5fbcbf3734fc8a60817a4635453dfe
chilir <minghao_li@outlook.com>
2025-01-29 22:47:38 -0800

    chore: reorganize interfaces
    Signed-off-by: chilir <minghao_li@outlook.com>

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -2,9 +2,50 @@
 
 import type { Database } from "bun:sqlite";
 import type { Mutex } from "async-mutex";
-import type { Message as DiscordMessage } from "discord.js";
+import type {
+  Client as DiscordClient,
+  Message as DiscordMessage,
+} from "discord.js";
+import type Exa from "exa-js";
 import type OpenAI from "openai";
 
+// Database related interfaces
+export interface dbCache {
+  cache: Map<string, DbCacheEntry>;
+  mutex: Mutex;
+  evictionInterval: ReturnType<typeof setInterval> | undefined;
+}
+
+export interface DbCacheEntry {
+  filePath: string;
+  db: Database;
+  lastAccessed: number;
+  refCount: number;
+}
+
+// Client interfaces
+export interface ChatThymeClients {
+  modelClient: OpenAI;
+  discordClient: DiscordClient;
+  exaClient: Exa | undefined;
+}
+
+// Discord chat thread management interfaces
+export interface ChatIdExistence {
+  exists: number; // 1 if exists, 0 if not
+}
+export interface ChatThreadInfo {
+  chatId: string;
+  userId: string;
+  modelOptions: Partial<ChatParameters>;
+}
+
+export interface ChatMessageQueue {
+  queue: DiscordMessage[];
+  stopSignal: boolean;
+}
+
+// Core chat interfaces
 export interface ChatPrompt {
   modelName: string;
   messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[];
@@ -27,32 +68,4 @@ interface ErrorResponse {
 
 export interface ChatResponse extends OpenAI.Chat.ChatCompletion {
   error?: ErrorResponse;
-}
-
-export interface ChatIdExistence {
-  exists: number; // 1 if exists, 0 if not
-}
-
-export interface dbCache {
-  cache: Map<string, DbCacheEntry>;
-  mutex: Mutex;
-  checkIntervalId: ReturnType<typeof setInterval> | undefined;
-}
-
-export interface DbCacheEntry {
-  filePath: string;
-  db: Database;
-  lastAccessed: number;
-  refCount: number;
-}
-
-export interface ChatThreadInfo {
-  chatId: string;
-  userId: string;
-  modelOptions: Partial<ChatParameters>;
-}
-
-export interface ChatMessageQueue {
-  queue: DiscordMessage[];
-  stopSignal: boolean;
 }
